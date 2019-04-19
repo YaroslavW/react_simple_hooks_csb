4f937d3220b0730d921f3a8fb5f0bf72b01499d8
Yaroslav <doctor355@yahoo.com>
2019-04-19 14:46:03 +0300

    add clear buttons and own state for other button

--- src/Components/UseState_1.js
+++ src/Components/UseState_1.js
@@ -2,11 +2,19 @@ import React, {useState} from 'react';
 
 const UseState_1 = () => {
     const [age, setAge] = useState(0);
+    const [siblings, setSiblings] = useState(0);
     const handleIncrease = () => setAge( age + 1);
+    const handleIncrSiblings = () => setSiblings( siblings + 1);
+    const clearAge = () => setAge(0);
+    const clearSiblings = () => setSiblings(0);
     return(
         <div>
         <p>My age now is - {age} year old!</p>
+        <p>I have {siblings} siblings</p>
         <button onClick={handleIncrease}>Grow</button>
+        <button onClick={clearAge}>Clear age</button>
+        <button onClick = {handleIncrSiblings}>Add Siblings</button>
+        <button onClick={clearSiblings}>Clear siblings</button>
         </div>
     )
 } 
